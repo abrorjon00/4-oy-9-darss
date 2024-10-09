ba115f6afa5d604ba5be6349a02b685f7c4eb938
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-10-09 11:45:54 +0300

    9-dars

---
+++ app.js/app.js
@@ -0,0 +1,190 @@
+// 1-misol
+
+// let arr = [2, 3, 4, 5, 6, 9];
+// console.log(arr[0]);
+// console.log(arr[5]);
+
+// 2-misol
+
+// let arr= [2, 4, 5, 6, 7, 8, 9];
+
+// for (let i = 0; i <= arr[i]; i++) {
+//     if (i % 2 == 1) {
+//         console.log(arr[i]);
+        
+//     }
+// }
+
+// 3-misol
+
+// let arr = ["Olma", "olcha", "shaftoli"];
+// console.log(arr);
+
+// 4-misol
+
+// let arr = [2, 3, 5, 69,  7, 78, 6];
+// console.log(arr.length);
+
+// 5-misol
+
+// let arr = [2, 4, 5, 6];
+// arr.unshift("salom")
+// arr.push("hayr")
+// console.log(arr);
+
+
+// 6-misol
+
+// let arr = [2, 4, 5, 6, 7];
+// arr.pop();
+// console.log(arr);
+
+// 7-misol
+
+// let arr = [2, 4,  5, 6, 7,];
+// arr.unshift("salom");
+// console.log(arr);
+
+
+// 8-misol
+// let arr = [2, 4,  5, 6, 7,];
+// arr.shift();
+// console.log(arr);
+
+// 9-misol
+
+// let arr = [2, 4, 5, 6, 7];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+    
+// }
+
+
+
+
+// For loopga iod masalalar
+
+// 1-misol
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//   console.log(numbers[i]);
+// }
+
+// 2-misol
+
+// let fruits = ["olma", "behi", "nok", "shaftoli"];
+// for( let i = 0; i < fruits.length; i++) {
+//     console.log(fruits[i]);
+    
+// }
+
+// 3-misol
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 == 0) {
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+// 4-misol
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] % 2 == 1) {
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+// 5-misol
+
+// let numbers = [1, 2, -3, 4, -5, 6, 7, -8, 9, 10];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] > 0) {
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+
+// 6-misol
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//   console.log(numbers[i]);
+// }
+
+// 7-misol
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let sum = 0;
+// for (let i = 0; i <= numbers.length; i++) {
+//     sum += i
+// }
+// console.log(sum);
+
+// 8-misol
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for ( let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i] * 2);
+    
+// }
+
+// 9-misol
+// let numbers = [5, 12, 9, 34, 2, 18, 7, 15, 27, 6];
+
+// let max = numbers[0]; 
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+// }
+// console.log(max); 
+
+
+// 11-misol
+
+// let arr = [-2, 4, 4, 6, -3, -1];
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] < 0) {
+//         console.log(arr[i]);
+        
+//     }
+// } 
+
+// 12-misol
+
+// let arr = [2, -3, 4, 6, -6];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > 0) {
+//         sum += arr[i]
+//     }
+// }
+// console.log(sum);
+
+// 13-misol
+
+// let arr = [1, 2, 3, 5, 6, 9];
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr);
+    
+// }
+
+// 14-misol
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+// for (let i = numbers.length - 1; i >= 0; i--) {
+//   console.log(i); 
+// }
+
+
